8ecdebc19e0f12489f7d6ee9995076f7ab665e66
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 15:18:04 +0300

    Add files via upload

---
+++ Article/ArticleTests.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class ArticleTests
+{
+    private Article _article;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _article = new Article();
+    }
+    [Test]
+    public void Test_AddArticles_ReturnsArticleWithCorrectData()
+    {
+        // Arrange
+        string[] articles = new string[] { "Article Political PLamena_Koycheva", "War_In_Middle_East Content2 Ivan_Ivanov", "Healthier_Life Lifestyle Author3" };
+
+
+        // Act
+        Article result = _article.AddArticles(articles);
+        // Assert
+        Assert.That(result.ArticleList, Has.Count.EqualTo(3));
+        Assert.That(result.ArticleList[0].Title, Is.EqualTo("Article"));
+        Assert.That(result.ArticleList[1].Content, Is.EqualTo("Content2"));
+        Assert.That(result.ArticleList[2].Author, Is.EqualTo("Author3"));
+    }
+
+    [Test]
+    public void Test_GetArticleList_SortsArticlesByTitle()
+    {
+        string[] article =
+         {
+            "New_Hair_Style LifeStyle Nona",
+            "Amazing_Race LifeStyle Ivan",
+            "Forbidden Political Georgi"
+        };
+
+        _article = _article.AddArticles(article);
+        
+        string expected = $"Amazing_Race - LifeStyle: Ivan{Environment.NewLine}" +
+            $"Forbidden - Political: Georgi{Environment.NewLine}" +
+        $"New_Hair_Style - LifeStyle: Nona";
+            
+
+
+
+
+        // Act
+        string result = _article.GetArticleList(_article, "title");
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GetArticleList_ReturnsEmptyString_WhenInvalidPrintCriteria()
+    {
+        string[] article =
+       {
+            "New_Hair_Style LifeStyle Nona",
+            "Amazing_Race LifeStyle Ivan",
+            "Forbidden Political Georgi"
+        };
+
+        _article = _article.AddArticles(article);
+
+        string expected = string.Empty;
+
+
+
+
+
+        // Act
+        string result = _article.GetArticleList(_article, "popcorn");
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
---
+++ Person/PersonTests.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+namespace TestApp.UnitTests;
+
+public class PersonTests
+{
+    private Person _person;
+    [SetUp]
+
+    public void SetUp()
+    {
+        _person = new Person();
+    }
+
+    
+    [Test]
+    public void Test_AddPeople_ReturnsListWithUniquePeople()
+    {
+        // Arrange
+        string[] peopleData = { "Alice A001 25", "Bob B002 30", "Alice A001 35" };
+
+        Person person = new Person()
+        {
+            Name = "Alice",
+            Age = 35,
+            Id = "A001"
+
+        };
+
+        Person person1 = new Person()
+        {
+            Name = "Bob",
+            Age = 30,
+            Id = "B002"
+        };
+
+
+
+        List<Person> expectedPeopleList = new List<Person>();
+        expectedPeopleList.Add(person);
+        expectedPeopleList.Add(person1);
+             
+        // Act
+        List<Person> resultPeopleList = this._person.AddPeople(peopleData);
+
+        // Assert
+        Assert.That(resultPeopleList, Has.Count.EqualTo(2));
+        for (int i = 0; i < resultPeopleList.Count; i++)
+        {
+           Assert.That(resultPeopleList[i].Name, Is.EqualTo(expectedPeopleList[i].Name));
+           Assert.That(resultPeopleList[i].Id, Is.EqualTo(expectedPeopleList[i].Id));
+          Assert.That(resultPeopleList[i].Age, Is.EqualTo(expectedPeopleList[i].Age));
+        }
+    }
+
+    [Test]
+    public void Test_GetByAgeAscending_SortsPeopleByAge()
+    {
+        // Arrange
+        List<Person> person = new List<Person>()
+        { new Person()  {Name = "Plamena", Id = "0001", Age = 33},
+        new Person() {Name = "Zdravko", Id = "0002", Age = 33},
+        new Person () {Name = "Stefan", Id = "789", Age = 5},
+        new Person () {Name = "Dimana", Id = "123", Age = 3},
+        };
+
+        string expected = $"Dimana with ID: 123 is 3 years old.{Environment.NewLine}" +
+            $"Stefan with ID: 789 is 5 years old.{Environment.NewLine}" +
+            $"Plamena with ID: 0001 is 33 years old.{Environment.NewLine}" +
+            $"Zdravko with ID: 0002 is 33 years old.";
+
+        // Act
+        string result = this._person.GetByAgeAscending(person);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
