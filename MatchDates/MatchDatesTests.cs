eeca8c844409ec6125b211962b2572e924eecb7d
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 15:13:39 +0300

    Add files via upload

---
+++ MatchDates/MatchDatesTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class MatchDatesTests
+{
+    // TODO: finish the test
+    [Test]
+    public void Test_Match_ValidDate_ReturnsExpectedResult()
+    {
+        // Arrange
+        string date = "31-Dec-2022";
+        string expected = "Day: 31, Month: Dec, Year: 2022";
+
+        // Act
+        string result = MatchDates.Match(date);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    // TODO: finish the test
+    [Test]
+    public void Test_Match_NoMatch_ReturnsEmptyString()
+    {
+        // Arrange
+        string date = "Invalid date format";
+        string expected = string.Empty;
+
+        // Act
+        string result = MatchDates.Match(date);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Match_MultipleMatches_ReturnsFirstMatch()
+    {
+        // Arrange
+        string date = "31-Dec-2022, 31-Dec-2022, 31-Dec-2022";
+        string expected = "Day: 31, Month: Dec, Year: 2022";
+
+        // Act
+        string result = MatchDates.Match(date);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Match_EmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        string date = string.Empty;
+        string expected = string.Empty;
+
+        // Act
+        string result = MatchDates.Match(date);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Match_NullInput_ThrowsArgumentException()
+    {
+        // Arrange
+        string date = null;
+
+
+        // Act
+
+
+        // Assert
+        Assert.That(() => MatchDates.Match(date), Throws.TypeOf<ArgumentException>());
+    }
+}
---
+++ MatchUrls/MatchUrlsTests.cs
@@ -0,0 +1,75 @@
+﻿using NUnit.Framework;
+
+using System.Collections.Generic;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TestApp.UnitTests;
+
+public class MatchUrlsTests
+{
+    // TODO: finish the test
+    [Test]
+    public void Test_ExtractUrls_EmptyText_ReturnsEmptyList()
+    {
+        // Arrange
+        string text = "";
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    // TODO: finish the test
+    [Test]
+    public void Test_ExtractUrls_NoUrlsInText_ReturnsEmptyList()
+    {
+        // Arrange
+        string text = "plamena, ivona, maria";
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_ExtractUrls_SingleUrlInText_ReturnsSingleUrl()
+    {
+        // Arrange
+        string text = "plamena, ivona, maria, https://www.myspace.com";
+        List<string> expected = new List<string> { "https://www.myspace.com" };
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_ExtractUrls_MultipleUrlsInText_ReturnsAllUrls()
+    {
+        // Arrange
+        string text = " https://www.plamena.com, https://www.ivona.org, maria, https://www.myspace.com";
+        List<string> expected = new List<string> { "https://www.plamena.com", "https://www.ivona.org", "https://www.myspace.com" };
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_ExtractUrls_UrlsInQuotationMarks_ReturnsUrlsInQuotationMarks()
+    {
+        // Arrange
+        string text = "\"https://www.nap.bg\", \"https://example.com\"";
+        List<string> expected = new List<string> { "https://www.nap.bg", "https://example.com" };
+
+        // Act
+        List<string> result = MatchUrls.ExtractUrls(text);
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
