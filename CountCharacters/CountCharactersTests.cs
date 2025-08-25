f9ed378f06487e23c5bc1ce46ab2998b0b1d0ec3
Plamena Koycheva <prodanowa@gmail.com>
2025-08-25 15:04:30 +0300

    Add files via upload

---
+++ CountCharacters/CountCharactersTests.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+
+namespace TestApp.Tests;
+
+public class CountCharactersTests
+{
+    [Test]
+    public void Test_Count_WithEmptyList_ShouldReturnEmptyString()
+    {
+        // Arrange
+        List<string> input = new();
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    // TODO: finish test
+    [Test]
+    public void Test_Count_WithNoCharacters_ShouldReturnEmptyString()
+    {
+        // Arrange
+        List<string> input = new List<string> { "", "", "" };
+        string expected = string.Empty;
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Count_WithSingleCharacter_ShouldReturnCountString()
+    {
+        List<string> input = new List<string> { "4" };
+        string expected = "4 -> 1";
+                         
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Count_WithMultipleCharacters_ShouldReturnCountString()
+    {
+        List<string> input = new List<string> { "ab", "bc", "ac" };
+        string expected = "a -> 2" +
+            Environment.NewLine
+            + "b -> 2" 
+         +   Environment.NewLine
+            + "c -> 2";
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Count_WithSpecialCharacters_ShouldReturnCountString()
+    {
+        List<string> input = new List<string> { "ab@", "ab%" };
+        string expected = "a -> 2"
+                          + Environment.NewLine
+                          + "b -> 2"
+                          + Environment.NewLine
+                          + "@ -> 1"
+                          + Environment.NewLine
+                          + "% -> 1";
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
